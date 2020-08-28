e52ad1173a6878eec900a48744e8d5f55c6a1280
yokoro13 <yokoro1219@yahoo.co.jp>
2020-08-28 15:20:38 +0900

    Update previousをhasPreviousに変更

--- MLDPTerminal-iOS/models/TextBuffer.swift
+++ MLDPTerminal-iOS/models/TextBuffer.swift
@@ -10,13 +10,13 @@ import UIKit
 struct textAttr {
     var char: String        // 文字を保存する変数
     var color: UIColor      // 色を保存する変数
-    var previous: Bool      // 前に続く文字の有無(同一行内か)を表す変数
+    var hasPrevious: Bool      // 前に続く文字の有無(同一行内か)を表す変数
 
     // 初期化関数
-    init(char: String, color: UIColor, previous: Bool = true) {
+    init(char: String, color: UIColor, hasPrevious: Bool = true) {
         self.char = char
         self.color = color
-        self.previous = previous
+        self.hasPrevious = hasPrevious
     }
 }
 
